21400788bc30706b3c350aac27a47ac3303c9401
SumitomShome <sumitomshome@gmail.com>
2020-10-25 14:11:42 +0530

    [Sumitom] Add: Method to create a queue

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,12 +5,17 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            linkedListStack.Display();
-            linkedListStack.IsEmpty();
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.Push(70);
+            //linkedListStack.Push(30);
+            //linkedListStack.Push(56);
+            //linkedListStack.Display();
+            //linkedListStack.IsEmpty();
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
             Console.ReadKey();
         }
     }
